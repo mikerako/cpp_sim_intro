77fb94b7084a9afbac8fe88016a5f61c31b81bdf
Michael Rakowiecki <mrako@umich.edu>
2022-02-07 22:56:56 -0500

    Have a controller for end effector force running

--- quad_leg/run_quad_leg_openloop.cpp
+++ quad_leg/run_quad_leg_openloop.cpp
@@ -11,8 +11,6 @@
 #include <drake/systems/controllers/pid_controlled_system.h>
 #include "drake/common/drake_assert.h"
 #include "drake/common/find_resource.h"
-// #include "drake/common/text_logging_gflags.h"
-// #include "drake/geometry/geometry_visualization.h"
 #include "drake/geometry/scene_graph.h"
 #include "drake/lcm/drake_lcm.h"
 #include "drake/multibody/parsing/parser.h"
@@ -23,16 +21,8 @@
 #include "drake/systems/primitives/constant_vector_source.h"
 
 #include "drake/common/find_resource.h"
-// #include "drake/common/is_approx_equal_abstol.h"
-// #include "drake/examples/quadrotor/quadrotor_geometry.h"
-// #include "drake/examples/quadrotor/quadrotor_plant.h"
 #include "drake/geometry/drake_visualizer.h"
-// #include "drake/lcm/drake_lcm.h"
-// #include "drake/systems/analysis/simulator.h"
-// #include "drake/systems/framework/diagram.h"
-// #include "drake/systems/framework/diagram_builder.h"
 
-// DEFINE_int32(simulation_trials, 10, "Number of trials to simulate.");
 DEFINE_double(simulation_real_time_rate, 1.0, "Real time rate");
 DEFINE_double(simulation_time, 7.0, "How long to simulate the system for");
 DEFINE_double(max_time_step, 1.0e-4, "Simulation timstep used for integrator.");
@@ -43,20 +33,11 @@ DEFINE_double(initial_height, 1.0, "initial model height");
 
 
 namespace drake {
-// using systems::DiagramBuilder;
-// using systems::Simulator;
-// using systems::Context;
-// using systems::ContinuousState;
-// using systems::VectorBase;
-
-// namespace examples {
 namespace quadleg {
-// namespace {
 
 static const char* const kQuadlegURDFPath = 
   "../models/urdf/leg.urdf"; // <--relative_path
   // "/home/mrako/Documents/EMBIR/cpp_sim_intro/models/urdf/leg.urdf"; <--global_path
-
 
 
 /// Prints contact results to terminal at every timestep of simulation
@@ -64,7 +45,7 @@ static const char* const kQuadlegURDFPath =
 /// @system
 /// name: ContactObserver
 /// input_ports:
-/// - state
+/// - contact_results from MBP
 /// output_ports:
 /// - none
 /// @endsystem
@@ -74,25 +55,16 @@ static const char* const kQuadlegURDFPath =
 class ContactObserver final : public systems::LeafSystem<double> {
 public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(ContactObserver);
-  // ~ContactObserver() final;
 
-  // /// Returns the frame of the geometry registered with a SceneGraph.  This can
-  // /// be useful, e.g., if one would like to add a camera to the quadrotor.
-  // geometry::FrameId get_frame_id() const { return frame_id_; }
-
-  /// Creates, adds, and connects a QuadrotorGeometry system into the given
-  /// `builder`.  Both the `quadrotor_state.get_system()` and `scene_graph`
-  /// systems must have been added to the given `builder` already.
+  /// Creates, adds, and connects a ContactObserver system into the given
+  /// `builder`.
   ///
-  /// The `scene_graph` pointer is not retained by the %QuadrotorGeometry
-  /// system.  The return value pointer is an alias of the new
-  /// %QuadrotorGeometry system that is owned by the `builder`.
+  /// The return value pointer is an alias of the new
+  /// %ContactObserver system that is owned by the `builder`.
   static const ContactObserver* AddToBuilder(
       systems::DiagramBuilder<double>& builder,
-      const systems::OutputPort<double>& plant_contact_port,
-      geometry::SceneGraph<double>& scene_graph) {
+      const systems::OutputPort<double>& plant_contact_port) {
         DRAKE_THROW_UNLESS(&builder != nullptr);
-        DRAKE_THROW_UNLESS(&scene_graph != nullptr);
 
         auto contact_observer = builder.AddSystem(
             std::unique_ptr<ContactObserver>(
@@ -104,79 +76,181 @@ public:
         return contact_observer;
   }
 
- private:
-  // explicit QuadrotorGeometry(geometry::SceneGraph<double>*);
-  // void OutputGeometryPose(const systems::Context<double>&,
-  //                         geometry::FramePoseVector<double>*) const;
-
+private:
   ContactObserver() {
     DeclareAbstractInputPort("contact", Value<multibody::ContactResults<double>>());
-    // this->DeclareAbstractOutputPort(
-    // DeclareAbstractOutputPort("publishing", &(this->PrintContactResults));
     DeclarePerStepPublishEvent(&ContactObserver::PrintContactResults);
     std::cout << "Constructed the ContactObserver\n";
   }
 
   systems::EventStatus PrintContactResults(const systems::Context<double>& context) const {
-      // DRAKE_DEMAND(frame_id_.is_valid());
+    const auto& contact_results = get_input_port(0).Eval<multibody::ContactResults<double>>(context); // VectorX<double>
 
-      // const multibody::ContactResults<double>& contact_results = get_input_port(0).Eval(context); // VectorX<double>
-      const auto& contact_results = get_input_port(0).Eval<multibody::ContactResults<double>>(context); // VectorX<double>
+    // std::cout << "num point pair: " << contact_results.num_point_pair_contacts() <<
+    //               "\t|\tnum hydroelastic: " << contact_results.num_hydroelastic_contacts() << "\n";
+
+    // int num_of_contacts = contact_results.num_point_pair_contacts();
+    // for(int i = 0; i < num_of_contacts; ++i){
+    //   multibody::PointPairContactInfo<double> pair_i = contact_results.point_pair_contact_info(i);
+    //   std::cout << "Contact force for pair " << i << " is: " << pair_i.contact_force() << "\n";
+    // }
+    return systems::EventStatus::Succeeded();
+  }
+};
+
+
+/// Custom controller that computes torque required to achieve a 
+/// desired ground reaction force (GRF)
+///
+/// @system
+/// name: GRFController
+/// input_ports:
+/// - state
+/// - reference GRF to achieve
+/// output_ports:
+/// - control inputs for multibodyplant (MBP)
+/// @endsystem
+///
+/// This class has no public constructor; instead use the AddToBuilder() static
+/// method to create and add it to a DiagramBuilder directly.
+class GRFController final : public systems::LeafSystem<double> {
+public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(GRFController);
+
+  /// Creates, adds, and connects a GRFController system into the given
+  /// `builder`.
+  ///
+  /// The return value pointer is an alias of the new
+  /// %GRFController system that is owned by the `builder`.
+  static const GRFController* AddToBuilder(
+      systems::DiagramBuilder<double>& builder,
+      // const systems::OutputPort<double>& plant_state_port,
+      // const systems::InputPort<double>& plant_actuation_port,
+      const systems::ConstantVectorSource<double>* reference,
+      const multibody::MultibodyPlant<double>* mbp) {
+        DRAKE_THROW_UNLESS(&builder != nullptr);
+
+        auto grf_controller = builder.AddSystem(
+            std::unique_ptr<GRFController>(
+                new GRFController(mbp)));
+        builder.Connect(
+            mbp->get_state_output_port(),
+            grf_controller->get_input_port(0));
+        builder.Connect(
+            reference->get_output_port(),
+            grf_controller->get_input_port(1));
+        builder.Connect(
+            grf_controller->get_output_port(0),
+            mbp->get_actuation_input_port());
+
+        return grf_controller;
+  }
+
+ private:
+  GRFController(const multibody::MultibodyPlant<double>* mbp) 
+    : plant(mbp) {
+    // Define controller input ports
+    DeclareVectorInputPort("state", mbp->num_multibody_states());
+    DeclareVectorInputPort("reference_grf", 3); //size 3 vector of forces at foot wrt foot frame
+
+    // Define controller output ports
+    DeclareVectorOutputPort("output_torques", mbp->num_actuators(), &GRFController::CalcGeneralizedOutput);
+    std::unique_ptr<systems::Context<double>> multibody_plant_context = plant->CreateDefaultContext();
+
+    // Declare cache entry for the multibody plant context.
+    multibody_plant_context_cache_index =
+        this->DeclareCacheEntry(
+                "multibody_plant_context_cache",
+                *multibody_plant_context,
+                &GRFController::SetMultibodyContext,
+                {this->input_port_ticket(
+                    get_input_port(0).get_index())})
+            .cache_index();
+
+    std::cout << "Constructed the GRFController\n";
+  }
+
+
+  void SetMultibodyContext(const systems::Context<double>& context,
+                           systems::Context<double>* multibody_plant_context) const {
+    // const VectorX<double>& x = get_input_port_estimated_state().Eval(context);
+    const VectorX<double>& x = get_input_port(0).Eval(context);
+
+    // if (this->is_pure_gravity_compensation()) {
+    //   // Velocities remain zero, as set in the constructor, for pure gravity
+    //   // compensation mode.
+    //   const VectorX<T> q = x.head(multibody_plant_->num_positions());
+    //   multibody_plant_->SetPositions(multibody_plant_context, q);
+    // } else {
+      // Set the plant positions and velocities.
+      plant->SetPositionsAndVelocities(multibody_plant_context, x);
+    // }
+  }
+
+
+  void CalcGeneralizedOutput(const systems::Context<double>& context, systems::BasicVector<double>* output) const {
+      auto full_state = get_input_port(0).Eval(context);
+      auto desired_grf = get_input_port(1).Eval(context);
+      // plant.SetPositionsAndVelocities(full_state)
+
+      // const auto& contact_results = get_input_port(0).Eval<multibody::ContactResults<double>>(context); // VectorX<double>
       // std::cout << "Size of Eval: " << contact_results.size() << "\n";
 
-      std::cout << "num point pair: " << contact_results.num_point_pair_contacts() <<
-                   "\t|\tnum hydroelastic: " << contact_results.num_hydroelastic_contacts() << "\n";
+      // plant.CalcGravityGeneralizedForces(context);
 
-      int num_of_contacts = contact_results.num_point_pair_contacts();
-      for(int i = 0; i < num_of_contacts; ++i){
-        multibody::PointPairContactInfo<double> pair_i = contact_results.point_pair_contact_info(i);
-        std::cout << "Contact force for pair " << i << " is: " << pair_i.contact_force() << "\n";
-      }
-      return systems::EventStatus::Succeeded();
+      // # Compute foot jacobian
+      //   J = self.plant.CalcJacobianSpatialVelocity(self.context,
+      //                                              JacobianWrtVariable.kV,
+      //                                              self.foot_frame,
+      //                                              np.zeros(3),
+      //                                              self.plant.world_frame(),
+      //                                              self.plant.world_frame())
+
+      const int num_generalized_positions = plant->num_positions();
+      Matrix6X<double> Jq_V_WEp(6, num_generalized_positions);
+
+      // auto& multibodyplant = *plant;
+
+      const auto& multibody_plant_context =
+          this->get_cache_entry(multibody_plant_context_cache_index)
+              .Eval<systems::Context<double>>(context);
+
+      VectorX<double> tau_g = -plant->CalcGravityGeneralizedForces(multibody_plant_context);
+      std::cout << tau_g << "\n\n";
+
+      // systems::Context<double>& plant_context = systems::Diagram<double>::GetMutableSubsystemContext(plant, context);
+      plant->CalcJacobianSpatialVelocity(multibody_plant_context,//plant->get_context(), 
+                                         multibody::JacobianWrtVariable::kQDot, 
+                                         plant->GetFrameByName("foot_link"), 
+                                         Vector3<double>{0.0,0.0,0.0},
+                                         plant->GetFrameByName("base_link"), 
+                                         plant->GetFrameByName("base_link"),
+                                         &Jq_V_WEp);
+
+      std::cout << Jq_V_WEp << "\n\n";
+
+      //torque = Ji.T * Ri.T * fi;
+      auto Ji = Jq_V_WEp.bottomRightCorner<3,2>();
+      // std::cout << Ji << "\n\n";
+      // std::cout << desired_grf << "\n\n";
+      // std::cout << Ji.transpose() << "\n\n";
+
+
+      auto torque = Ji.transpose() * desired_grf;
+      std::cout << "torque:" << torque << "\n\n";
+      std::cout << "torque+tau_g:" << torque + tau_g.tail(2) << "\n\n";
+      // set output
+      output->set_value(FLAGS_Kp_ * (torque + tau_g.tail(2)));
+      // output->set_value(Vector2<double>{20.0,0.0});
+      // output[0] = 0;
+      // output[1] = 0;
     }
 
-  // // Geometry source identifier for this system to interact with SceneGraph.
-  // geometry::SourceId source_id_{};
-  // // The id for the quadrotor body.
-  // geometry::FrameId frame_id_{};
+    const multibody::MultibodyPlant<double>* plant;
+    // std::unique_ptr<systems::Context<double>> multibody_plant_context;
+    systems::CacheIndex multibody_plant_context_cache_index;
 };
-//     : systems::LeafSystem<T>(systems::SystemTypeTag<QuadrotorPlant>{}),
-//       g_{9.81}, m_(m_arg), L_(L_arg), kF_(kF_arg), kM_(kM_arg), I_(I_arg) {
-//   // Four inputs -- one for each propellor.
-//   this->DeclareInputPort("propellor_force", systems::kVectorValued, 4);
-//   // State is x ,y , z, roll, pitch, yaw + velocities.
-//   auto state_index = this->DeclareContinuousState(12);
-//   this->DeclareStateOutputPort("state", state_index);
-//   // TODO(russt): Declare input limits.  R2 has @ 2:1 thrust to weight ratio.
-// }
 
-
-
-// std::unique_ptr<systems::AffineSystem<double>> StabilizingLQRController(
-//     const QuadrotorPlant<double>* quadrotor_plant,
-//     Eigen::Vector3d nominal_position) {
-//   auto quad_context_goal = quadrotor_plant->CreateDefaultContext();
-
-//   Eigen::VectorXd x0 = Eigen::VectorXd::Zero(12);
-//   x0.topRows(3) = nominal_position;
-
-//   // Nominal input corresponds to a hover.
-//   Eigen::VectorXd u0 = Eigen::VectorXd::Constant(
-//       4, quadrotor_plant->m() * quadrotor_plant->g() / 4);
-
-//   quadrotor_plant->get_input_port(0).FixValue(quad_context_goal.get(), u0);
-//   quad_context_goal->SetContinuousState(x0);
-
-//   // Setup LQR cost matrices (penalize position error 10x more than velocity
-//   // error).
-//   Eigen::MatrixXd Q = Eigen::MatrixXd::Identity(12, 12);
-//   Q.topLeftCorner<6, 6>() = 10 * Eigen::MatrixXd::Identity(6, 6);
-
-//   Eigen::Matrix4d R = Eigen::Matrix4d::Identity();
-
-//   return systems::controllers::LinearQuadraticRegulator(
-//       *quadrotor_plant, *quad_context_goal, Q, R);
-// }
 
 
 multibody::MultibodyPlant<double>* add_leg(systems::DiagramBuilder<double>& builder, geometry::SceneGraph<double>& scene_graph){
@@ -196,39 +270,27 @@ void add_ground(multibody::MultibodyPlant<double>* quadleg){
 }
 
 int do_main() {
-  // lcm::DrakeLcm lcm;
   DRAKE_DEMAND(FLAGS_simulation_time > 0);
 
+  // make a DiagramBuilder
   systems::DiagramBuilder<double> builder;
 
+  // make a SceneGraph
   geometry::SceneGraph<double>& scene_graph = 
     *builder.AddSystem<geometry::SceneGraph>();
   scene_graph.set_name("scene_graph");
 
   // //Load and parse the leg URDF file for the MultibodyPlant
-  // multibody::MultibodyPlant<double>* quadleg = 
-  //   builder.AddSystem<multibody::MultibodyPlant<double>>(FLAGS_max_time_step);
-  // quadleg->set_name("plant");
-  // quadleg->RegisterAsSourceForSceneGraph(&scene_graph);
   multibody::MultibodyPlant<double>* quadleg = add_leg(builder, scene_graph);
 
   multibody::Parser parser(quadleg);
-  // const std::string urdf_path = FindResourceOrThrow(kQuadlegURDFPath);
   const std::string urdf_path = kQuadlegURDFPath;
   multibody::ModelInstanceIndex plant_model_instance_index = 
     parser.AddModelFromFile(urdf_path);
   (void)plant_model_instance_index;
 
-
+  // add collision geometry and visuals for the ground
   add_ground(quadleg);
-  // const math::RigidTransform<double> X_BG(Vector3<double>{0.0, 0.0, 0.0});
-  // multibody::CoulombFriction<double> surface_friction = multibody::CoulombFriction(0.7, 0.5); //static_friction, dynamic_friction
-  // quadleg->RegisterCollisionGeometry(quadleg->world_body(),X_BG,geometry::HalfSpace(),"ground_collision",surface_friction);
-  // quadleg->RegisterVisualGeometry(quadleg->world_body(),X_BG,geometry::HalfSpace(),"ground_visual",Vector4<double>{0.15234375,0.95703125,0.421875,0.65});
-
-
-  // quadleg->AddJointActuator("a1", quadleg->GetJointByName("Actuator1")); //Use more descriptive names for actual robot
-  // quadleg->AddJointActuator("a2", quadleg->GetJointByName("Actuator2")); //Use more descriptive names for actual robot
 
   // Now the MultibodyPlant is complete
   quadleg->Finalize();
@@ -269,16 +331,18 @@ int do_main() {
   /// temp fix to remove the controller!
   auto desired_base_source = 
     builder.AddSystem<systems::ConstantVectorSource<double>>(
-      Eigen::VectorXd::Zero(quadleg->num_actuators()) //quadleg->num_multibody_states()
+      // Eigen::VectorXd::Zero(3) //Eigen::VectorXd::Zero(quadleg->num_actuators()) //quadleg->num_multibody_states()
+      Vector3<double>{0.0,0.0,50.0}
     );
-  builder.Connect(desired_base_source->get_output_port(),
-                  quadleg->get_actuation_input_port()); 
+  ///////////// builder.Connect(desired_base_source->get_output_port(),
+  /////////////                 quadleg->get_actuation_input_port()); 
   ///
+  GRFController::AddToBuilder(builder, desired_base_source, quadleg);
                  
 
 
   ContactObserver::AddToBuilder(
-      builder, quadleg->get_contact_results_output_port(), scene_graph);
+      builder, quadleg->get_contact_results_output_port());
 
 
 
@@ -309,7 +373,8 @@ int do_main() {
   // Set initial position
   Eigen::VectorXd positions = Eigen::VectorXd::Zero(quadleg->num_positions());
   positions[0] = 1.0;
-  positions[8] = 0.4;
+  positions[7] = -1.4;
+  positions[8] = 2.4;
   quadleg->SetPositions(&plant_context, positions);
   // Set intial velocity
   Eigen::VectorXd velocities = Eigen::VectorXd::Zero(quadleg->num_velocities());
@@ -321,6 +386,7 @@ int do_main() {
   systems::State<double>& plant_state =
       diagram->GetMutableSubsystemState(*quadleg, diagram_context.get());
   const math::RigidTransform<double> X_WB(
+      math::RollPitchYaw<double>(Vector3<double>{0.0,3.14,0.0}),
       Vector3<double>{0.0, 0.0, FLAGS_initial_height});
   quadleg->SetFreeBodyPose(
       plant_context, &plant_state, quadleg->GetBodyByName("base_link"), X_WB); 
